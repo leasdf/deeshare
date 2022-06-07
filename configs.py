d7da54b461945ee398ec94aadcfa53a995140fd9
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-07 18:45:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,22 +2,22 @@
 import os
 
 class Config(object):
-	API_HASH = "5d3831feb6752d0a6904accac2008250"
-	API_ID = 9325118
-	BOT_TOKEN = "5495370777:AAE_wxsdpo5RmDVFbgexUSxVxZnSIMGKeFs"
-	BOT_USERNAME = "pwfilestore_bot"
-        AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", 5122474448).split())
-	DB_CHANNEL = "-1001770626286"
-	BOT_OWNER = "1123564262"
-	DATABASE_URL = "mongodb+srv://LegendBoy:LegendBoy1234@cluster0.1kerb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-	LOG_CHANNEL = ""-1001770626286""
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
-	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-	ABOUT_BOT_TEXT = f"""
+    API_HASH = "5d3831feb6752d0a6904accac2008250"
+    API_ID = 9325118
+    BOT_TOKEN = "5495370777:AAE_wxsdpo5RmDVFbgexUSxVxZnSIMGKeFs"
+    BOT_USERNAME = "pwfilestore_bot"
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", 5122474448).split())
+    DB_CHANNEL = "-1001770626286"
+    BOT_OWNER = "1123564262"
+    DATABASE_URL = "mongodb+srv://LegendBoy:LegendBoy1234@cluster0.1kerb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
+    LOG_CHANNEL = ""-1001770626286""
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+    FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+    BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+    OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+    ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
 Send Me any Media or File.I can Work In Channel too Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
 
@@ -39,7 +39,7 @@ Send Me any Media or File.I can Work In Channel too Add Me to Channel with Edit 
 │
 ╰──────[ 😎 ]───────────⍟
 """
-	ABOUT_DEV_TEXT = f"""
+    ABOUT_DEV_TEXT = f"""
 🧑🏻‍💻 **Developer:** [LegendDeepanshu](https://t.me/LegendDeepanshu) 
 
 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐢𝐬 𝐒𝐮𝐩𝐞𝐫 𝐍𝐨𝐨𝐛😅.
@@ -50,7 +50,7 @@ Send Me any Media or File.I can Work In Channel too Add Me to Channel with Edit 
 
 [Donate Me](https://t.me/LegendDeepanshu) (Contact)
 """
-	HOME_TEXT = """
+    HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
 How to Use Bot & it's Benefits??
