a5bf55cbf274eb1893895b6ad40b76cf4f248c2c
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-07 18:47:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -11,7 +11,7 @@ class Config(object):
     BOT_OWNER = "1123564262"
     DATABASE_URL = "mongodb+srv://LegendBoy:LegendBoy1234@cluster0.1kerb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    LOG_CHANNEL = ""-1001770626286""
+    LOG_CHANNEL = "-1001770626286"
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
     FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
